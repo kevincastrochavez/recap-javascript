a8177b300f6f2b506c723af2d5040a1819024bb2
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-01-10 11:57:09 -0700

    finished coding challenge 3

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -14,11 +14,25 @@ function getBMI(mass, height) {
 const mark = getBMI(markWeight, markHeight);
 const john = getBMI(johnWeight, johnHeight);
 
+// Coding Challenge #2
+
 if (mark > john) {
-  console.log(`Mark's BMI (${mark}) is higher than John's (${john})`);
+  //   console.log(`Mark's BMI (${mark}) is higher than John's (${john})`);
 } else {
-  console.log(`Johns's BMI (${john}) is higher than Mark's (${mark})`);
+  //   console.log(`Johns's BMI (${john}) is higher than Mark's (${mark})`);
 }
 
 // console.log(mark);
 // console.log(john);
+
+// Coding Challenge #3
+const dolphins = [96, 108, 89];
+const koalas = [88, 91, 110];
+
+const dolphinsaverage =
+  dolphins.reduce((prev, curr) => prev + curr) / dolphins.length;
+console.log(dolphinsaverage);
+
+const koalasaverage =
+  koalas.reduce((prev, curr) => prev + curr) / koalas.length;
+console.log(koalasaverage);
